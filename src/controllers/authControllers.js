409c0aeb8fd0ba3c9285de51ba9f6b60237d111b
Petya Ivanchova <pepivanchova@yahoo.com>
2023-02-05 18:18:53 +0200

    Send cookie to client

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -14,7 +14,8 @@ router.post('/login', async (req,res) => {
 
    try{
     const token = await authService.login({username, password});
-    console.log(token);
+    
+    res.cookie('auth', token, { httpOnly: true });
    }
    catch(err){
     console.log(err);
